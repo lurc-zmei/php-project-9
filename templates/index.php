0f844c8459888780b3f8a9f0e348fa0df485ce46
WinDon <lurc_zmei@inbox.ru>
2026-06-19 09:28:21 +0300

    fix: template page /urls and vhost.conf

--- templates/index.php
+++ templates/index.php
@@ -1,32 +1,34 @@
-<h1><?= htmlspecialchars($title) ?></h1>
+<!--<h1><?= htmlspecialchars($title) ?></h1>
 <p><?= htmlspecialchars($message) ?></p>
+-->
 
 <?php if (isset($features) && is_array($features)): ?>
-    <h3>Основная информация:</h3>
-    <ul>
-        <?php foreach ($features as $feature): ?>
-            <li><?= htmlspecialchars($feature) ?></li>
-        <?php endforeach; ?>
-    </ul>
+  <h3>Основная информация:</h3>
+  <ul>
+    <?php foreach ($features as $feature): ?>
+      <li><?= htmlspecialchars($feature) ?></li>
+    <?php endforeach; ?>
+  </ul>
 <?php endif; ?>
 
+<h1 class="display-6 mb-4">Сайты</h1>
 
-
-Ключевая разметка таблицы со списком URL
-<table data-test="urls">
+<table class="table table-bordered table-hover" data-test="urls">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Имя</th>
-      <th>Дата создания</th>
-      <th>Код ответа</th>
+      <th scope="col">ID</th>
+      <th scope="col">Имя</th>
+      <th scope="col">Дата создания</th>
+      <th scope="col">Код ответа</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1</td>
-      <td><a href="/urls/1">https://example.com</a></td>
-      <td>2024-01-01</td>
+      <td>
+        <a href="/urls/5" aria-label="Просмотр сайта https://example.com">https://example.com</a>
+      </td>
+      <td>2026-06-15</td>
       <td>200</td>
     </tr>
   </tbody>
--- templates/layout.php
+++ templates/layout.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
+
+<head>
     <title>Страница</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </head>
- <body class="d-flex flex-column min-vh-100">
+</head>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid justify-content-start">
-        <a class="navbar-brand" href="/">Анализатор страниц</a>
-        <a class="nav-link link-light link-opacity-75" href="/urls">Сайты</a>
-    </div>
-</nav>
+<body class="d-flex flex-column min-vh-100">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid justify-content-start">
+            <a class="navbar-brand" href="/">Анализатор страниц</a>
+            <a class="nav-link link-light link-opacity-75" href="/urls">Сайты</a>
+        </div>
+    </nav>
 
 
-<main class="flex-grow-1">
+    <main class="flex-grow-1">
         <?php if (!empty($flash)): ?>
             <div class="container mt-3">
                 <?php foreach ($flash as $type => $messages): ?>
@@ -34,12 +36,13 @@
         </div>
     </main>
 
-    
-<hr class="border-secondary-subtle mb-0">
-<footer class="py-3">
-    <div class="container-lg text-center">
-        <a href="https://ru.hexlet.io" class="link-primary text-decoration-none">Hexlet</a>
-    </div>
-</footer>
-  </body>
+
+    <hr class="border-secondary-subtle mb-0">
+    <footer class="py-3">
+        <div class="container-lg text-center">
+            <a href="https://ru.hexlet.io" class="link-primary text-decoration-none">Hexlet</a>
+        </div>
+    </footer>
+</body>
+
 </html>
