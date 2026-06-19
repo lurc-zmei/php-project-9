6d2201771c2aa440e76322fc1c1ae9ebb2507d31
WinDon <lurc_zmei@inbox.ru>
2026-06-19 15:19:54 +0300

    feat: add connect to BD and route for prepared insert

--- public/index.php
+++ public/index.php
@@ -5,17 +5,33 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use DI\Container;
 use Slim\Views\PhpRenderer;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-// Создание контейнера PHP-DI
 $container = new Container();
 
 $container->set(PhpRenderer::class, new PhpRenderer(__DIR__ . '/../templates', [], 'layout.php'));
 
+$container->set(PDO::class, function () {
+    $dsn = "pgsql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}";
 
-// Создаём объект приложения Slim и передаём в него контейнер
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    try {
+        return new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $options);
+    } catch (\PDOException $e) {
+        die("Ошибка подключения к БД: " . $e->getMessage());
+    }
+});
+
+
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
@@ -27,10 +43,20 @@ $app->get('/', function (Request $request, Response $response) use ($container) 
 })->setName('home');
 
 
-
 $app->get('/urls', function (Request $request, Response $response) use ($container) {
     return $container->get(PhpRenderer::class)->render($response, 'index.php');
 })->setName('urls.index');
+
+
+$app->post('/', function (Request $request, Response $response) use ($container) {
+    $data = $request->getParsedBody();
+    $url = $data['url'] ?? '';
+    $pdo = $container->get(PDO::class);
+    $sql = 'INSERT INTO urls (name) VALUES (:url)';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['url' => $url]);
+    return $response->withHeader('Location', '/')->withStatus(302);
+});
 
 
 $app->run();
